dd473a45c9632c8e2e67906c8ed3b6be7b128002
SSLiza <shajedasultanaliza2002@gmail.com>
2026-09-13 18:59:13 +0600

    Homepage Created

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,22 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Home from "./pages/Home";
 import About from './pages/About';
+import MainLayout from './layout/MainLayout';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About/>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import { useState } from "react";
 
 const Home = () => {
+    const[click, setClick] = useState("");
+    console.log(click);
     return (
         <div>
-            <h1>Welcome to the Home Page</h1>
+            <h1 className='text-6xl font-bold text-blue-200'>NextLevel<span className='text-blue-500'> Weather App</span></h1>
+            <p className='text-lg text-gray-300 text-center py-4'>Check the weather in your city!</p>
+            <div className='flex justify-center items-center'>
+                <button 
+                type="button"
+                onClick={() => setClick("clicked")}
+                className='bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded-3xl'>Check Weather</button>
+            </div>
         </div>
     );
 };
